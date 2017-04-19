eeeab0dd45506f6840edd1f9a5f2241cfca9fab9
liuna <liuna@xiaoshouyi.com>
2017-04-19 18:18:27 +0800

    gulp css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    minifycss = require('gulp-minify-css');
+
+gulp.task('platformCss', function() {
+    return gulp.src(['app/styles/sources/*.css'], {
+            cwd: '.',
+            base: '.'
+    })
+    .pipe(concat('all.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('app/styles/target/'));
+});
+
+gulp.task('watchPlatformCss', function() {
+    return gulp.watch('app/styles/sources/*.css', ['platformCss'])
+});
 
 gulp.task('default', function() {
     
