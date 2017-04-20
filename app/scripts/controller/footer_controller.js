14e614bbf62fc43577b93d6cf37e2caaa6c58d99
liuna <liuna@xiaoshouyi.com>
2017-04-20 13:37:46 +0800

    ''

---
+++ app/scripts/controller/footer_controller.js
@@ -0,0 +1,20 @@
+define(['angular'], function(angular) {
+    angular.module("webapp").controller("FooterController", function ($scope) {
+     
+        $scope.submitFeedback = function(action) {
+            //good or bad...
+            if (action) {
+                //good
+     
+                //submit using $http or service
+     
+            }
+            else {
+                //bad
+     
+                //submit using $http or service
+            }
+        }
+     
+    });
+});
---
+++ app/scripts/directive/footer_directive.js
@@ -0,0 +1,11 @@
+//define(['angular'], function(angular) {
+    /*angular.module("webapp").directive("footer", function() {
+      return {
+        restrict: 'A',
+        templateUrl: '/partials/footer.html',
+        scope: true,
+        transclude : false,
+        controller: 'FooterController'
+      };
+    });*/
+//});
